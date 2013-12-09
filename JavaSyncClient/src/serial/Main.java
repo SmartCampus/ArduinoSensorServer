abea8530c62978e486330a113f14517ac8ed81ef
Jean <jean.oudot.06@gmail.com>
2013-12-09 14:16:41 +0100

    Code update

---
+++ JavaSyncClient/src/serial/Main.java
@@ -0,0 +1,51 @@
+
+package serial;
+
+/**
+ * Main class of program execution.
+ * 
+ * @author  Jean Oudot - IUT Nice / Sophia Antipolis - S4D
+ * @version 1.0.0
+ */
+public class Main
+{
+
+
+/**
+ * Program main.
+ * 
+ * @param args       Program argument.
+ * @throws Exception Creation error.
+ */
+public static void main(String[] args)
+throws Exception
+{
+   // Prepare to get Arduino datas and Arduino informations.
+   final InformationReceiver ir = new InformationReceiver();
+   final DataReceiver dr = new DataReceiver();
+
+   // Create a new Arduino interface (redefine the abstract methods).
+   ArduinoInterface ai = new ArduinoInterface(args[0])
+   {
+
+      public void sensorDataReceived(String data)
+      {
+         dr.writeData(data, "dataReceived.json");
+      }
+
+
+      public void infoReceived(String data)
+      {
+         // Log the received informations. 
+         ir.informationToLog(data);
+      }
+   };
+
+   // Testing add command.
+   System.out.println("Send file command.");
+   ai.execFileCommand("command1");
+   //ai.execCommand("add temperature2 3 4");
+   System.out.println("File command executed.");
+}
+
+}
